3546a55318bcd60a0ee6d66768836653107674d7
pranavdodiya <pranavdodiya72@gmail.com>
2024-12-19 17:09:48 +0530

    Added test for handling single number input

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.new.add('')).to eq(0)
     end
+    
+		# Returns the number itself for a single input
+		it 'returns the number itself for a single number' do
+			expect(StringCalculator.new.add("1")).to eq(1)
+		end
   end
 end
