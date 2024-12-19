5806c19d86fd092565894f91684de604cf2b5c17
pranavdodiya <pranavdodiya72@gmail.com>
2024-12-19 19:22:56 +0530

    Refactor StringCalculator for cleaner methods

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -4,28 +4,43 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    delimiter = ",|\n"
+    delimiter = extract_delimiters(numbers)
+    numbers = numbers.split("\n", 2).last if numbers.start_with?('//')
 
-    # Check if a custom delimiter is specified in the format //[*] or //[*][*]
-    if numbers.start_with?('//[')
-      delimiter = numbers.match(%r{//\[(.*?)\]\n})[1] # Extract delimiter from the string
-      delimiter = Regexp.escape(delimiter) # Escape any special characters in the delimiter
-      numbers = numbers.split("\n", 2).last
-    elsif numbers.start_with?('//')
-      delimiter = Regexp.escape(numbers[2]) # Single character delimiter
-      numbers = numbers.split("\n", 2).last
-    end
-
-    # Use the delimiter to split the numbers string
-    nums = numbers.split(/#{delimiter}/).map(&:to_i)
-
-    # Reject numbers greater than 1000
-    nums = nums.reject { |n| n > 1000 }
-
-    # Check for negative numbers
-    negatives = nums.select(&:negative?)
-    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+    nums = split_numbers(numbers, delimiter)
+    nums = reject_numbers_greater_than_1000(nums)
+    check_for_negative_numbers(nums)
 
     nums.sum
+  end
+
+  private
+
+  def extract_delimiters(numbers)
+    if numbers.start_with?('//[')
+      extract_multiple_delimiters(numbers)
+    elsif numbers.start_with?('//')
+      Regexp.escape(numbers[2]) # Single character delimiter
+    else
+      ",|\n" # Default delimiters
+    end
+  end
+
+  def extract_multiple_delimiters(numbers)
+    delimiters = numbers.match(%r{//\[(.*?)\]\n})[1] # Extract all delimiters inside []
+    delimiters.split('][').map { |delim| Regexp.escape(delim) }.join('|') # Combine them into a regex pattern
+  end
+
+  def split_numbers(numbers, delimiter)
+    numbers.split(/#{delimiter}/).map(&:to_i)
+  end
+
+  def reject_numbers_greater_than_1000(numbers)
+    numbers.reject { |n| n > 1000 }
+  end
+
+  def check_for_negative_numbers(numbers)
+    negatives = numbers.select(&:negative?)
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
