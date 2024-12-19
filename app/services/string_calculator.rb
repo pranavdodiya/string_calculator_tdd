7156c840fa88eb66eaf3db0a0a3d65bb7ab2aa7b
pranavdodiya <pranavdodiya72@gmail.com>
2024-12-19 19:10:39 +0530

    Rubocop fixes

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
     delimiter = ",|\n"
-    
+
     # Check if a custom delimiter is specified in the format //[*] or //[*][*]
-    if numbers.start_with?("//[")
+    if numbers.start_with?('//[')
       delimiter = numbers.match(%r{//\[(.*?)\]\n})[1] # Extract delimiter from the string
       delimiter = Regexp.escape(delimiter) # Escape any special characters in the delimiter
       numbers = numbers.split("\n", 2).last
-    elsif numbers.start_with?("//")
+    elsif numbers.start_with?('//')
       delimiter = Regexp.escape(numbers[2]) # Single character delimiter
       numbers = numbers.split("\n", 2).last
     end
@@ -19,9 +21,9 @@ class StringCalculator
 
     # Reject numbers greater than 1000
     nums = nums.reject { |n| n > 1000 }
-    
+
     # Check for negative numbers
-    negatives = nums.select { |n| n < 0 }
+    negatives = nums.select(&:negative?)
     raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
 
     nums.sum
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StringCalculator do
@@ -5,43 +7,43 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.new.add('')).to eq(0)
     end
-    
-		# Returns the number itself for a single input
-		it 'returns the number itself for a single number' do
-			expect(StringCalculator.new.add("1")).to eq(1)
-		end
 
-		# Returns the sum for two numbers separated by a comma
-		it 'returns the sum for two numbers separated by a comma' do
-			expect(StringCalculator.new.add("1,2")).to eq(3)
-		end
-
-		# Handles summing a large number of numbers
-		it "handles a large number of numbers" do
-      numbers = (1..1000).to_a.join(",")
-      expect(StringCalculator.new.add(numbers)).to eq(500500)
+    # Returns the number itself for a single input
+    it 'returns the number itself for a single number' do
+      expect(StringCalculator.new.add('1')).to eq(1)
     end
 
-		# Handles new lines between numbers in addition to commas
-		it 'handles new lines between numbers' do
-			expect(StringCalculator.new.add("1\n2,3")).to eq(6)
-		end
+    # Returns the sum for two numbers separated by a comma
+    it 'returns the sum for two numbers separated by a comma' do
+      expect(StringCalculator.new.add('1,2')).to eq(3)
+    end
 
-		# Supports custom delimiters specified at the beginning of the string
-		it 'supports custom delimiters' do
-			expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
-		end
+    # Handles summing a large number of numbers
+    it 'handles a large number of numbers' do
+      numbers = (1..1000).to_a.join(',')
+      expect(StringCalculator.new.add(numbers)).to eq(500_500)
+    end
 
-		# Raises an exception if negative numbers are included in the input
-		it 'raises an exception for negative numbers' do
-			expect { StringCalculator.new.add("1,-2,3,-4") }.to raise_error("negative numbers not allowed: -2, -4")
-		end
+    # Handles new lines between numbers in addition to commas
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+    end
+
+    # Supports custom delimiters specified at the beginning of the string
+    it 'supports custom delimiters' do
+      expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+    end
+
+    # Raises an exception if negative numbers are included in the input
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.new.add('1,-2,3,-4') }.to raise_error('negative numbers not allowed: -2, -4')
+    end
 
     # Ignores numbers greater than 1000 when calculating the sum
-		it 'ignores numbers greater than 1000' do
-      expect(StringCalculator.new.add("2,1001")).to eq(2)
-      expect(StringCalculator.new.add("1000,1001,2")).to eq(1002)
-      expect(StringCalculator.new.add("1001,1002,1003")).to eq(0)
+    it 'ignores numbers greater than 1000' do
+      expect(StringCalculator.new.add('2,1001')).to eq(2)
+      expect(StringCalculator.new.add('1000,1001,2')).to eq(1002)
+      expect(StringCalculator.new.add('1001,1002,1003')).to eq(0)
     end
 
     # Supports custom delimiters of any length, including multi-character delimiters
